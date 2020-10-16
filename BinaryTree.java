6150614c4e1f5f68b4252be8daf857f3a086bc42
sameer-ahmad0 <72499604+sameer-ahmad0@users.noreply.github.com>
2020-10-17 03:00:25 +0530

    Create BinaryTree.java

---
+++ BinaryTree.java
@@ -0,0 +1,39 @@
+class Node 
+{ 
+    int key; 
+    Node left, right; 
+  
+    public Node(int item) 
+    { 
+        key = item; 
+        left = right = null; 
+    } 
+} 
+class BinaryTree 
+{ 
+    // Root of Binary Tree 
+    Node root; 
+  
+    // Constructors 
+    BinaryTree(int key) 
+    { 
+        root = new Node(key); 
+    } 
+  
+    BinaryTree() 
+    { 
+        root = null; 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        BinaryTree tree = new BinaryTree(); 
+  
+        /*create root*/
+        tree.root = new Node(1); 
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(3); 
+        tree.root.left.left = new Node(4); 
+        
+         } 
+} 
